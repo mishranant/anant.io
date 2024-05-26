1105715e9be0936d30b67cd2532d62339beeaa0d
Anant Mishra <anant.mishra.student@gmail.com>
2024-05-26 23:49:18 +0530

    added images, format landing page

--- src/consts.ts
+++ src/consts.ts
@@ -3,3 +3,6 @@
 
 export const SITE_TITLE = 'CAT Person';
 export const SITE_DESCRIPTION = '1-on-1 Guidance';
+
+const CONTACT_MSG = "Hello, I want your 1-on-1 guidance regarding CAT, let me know when you are available :)";
+export const whatsappLink = `https://wa.me/9736605649?text=${encodeURIComponent(CONTACT_MSG)}`;
