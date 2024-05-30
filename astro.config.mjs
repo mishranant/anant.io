7ec4aa013c389e9b9ff691a89ffbd7a7c2ba4e6a
Anant Mishra <anant.mishra.student@gmail.com>
2024-05-30 22:49:17 +0530

    add adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
+import vercel from "@astrojs/vercel/serverless";
 
+// https://astro.build/config
 export default defineConfig({
-	output: 'server',
-	site: 'https://anant.com',
-	integrations: [mdx(), sitemap()],
+  output: 'server',
+  site: 'https://anant.com',
+  integrations: [mdx(), sitemap()],
+  adapter: vercel()
 });
